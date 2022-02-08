880eff7d43cb236ac29c74d0de1bc60ccff36aa7
Stjepan Vrbic <f0044q9@babylon1.thayer.dartmouth.edu>
2022-02-08 12:22:44 -0500

    kernel context switch comment

--- sample/kernel_context.c
+++ sample/kernel_context.c
@@ -1,6 +1,12 @@
 
 #include "include/hardware.h"
 
+/* The kernel stack initially maps 1 to 1 to phtsical addresses
+In the region 0 page table, the vpn correspond 1 to 1 to the psiginfo
+When we want to change context, we will loop through the vpns in the region 0 page table corresponding
+to the kernel stack
+And we will change the pfns to the pfns of the kernel stack table stored in the pcb of the
+process we are switching to. */
 KernelContext *KCSwitch(KernelContext *kc_in,
                         void *curr_pcb_p,
                         void *next_pcb_p)
@@ -12,8 +18,8 @@ KernelContext *KCSwitch(KernelContext *kc_in,
 }
 
 KernelContext *KCCopy(KernelContext *kc_in,
-                        void *new_pcb_p,
-                        void *not_used)
+                      void *new_pcb_p,
+                      void *not_used)
 {
     /* Copy kc_in into the new_pcb_p
     Copy contents of the kernel stack into the new process kernel stack
