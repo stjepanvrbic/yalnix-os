d6344584e99945bee4b04ab09a2a52c1d8a97c79
Selim Hassairi <f00430t@babylon5.thayer.dartmouth.edu>
2022-02-08 14:44:31 -0500

    without malloc

--- sample/hardware.c
+++ sample/hardware.c
@@ -2,9 +2,14 @@
 #include "../include/yalnix.h"
 #include <ykernel.h>
 
+// Global Variables.
+void *KERNEL_BRK;
+
 // Set up a way to track free frames (use a bit vector to track this)
 bit_vector_t mem_frames; // 0 for free, 1 for used.
 u_long n_frames;
+
+u_long brk_offset = 0; // keep track of allocated stuff.
 
 // Set up the Kernel Page Table.
 kernel_page_table_t kernel_page_table;
@@ -21,9 +26,13 @@ extern int SetKernelBrk(void *addr)
     // Have a flag to check whether vitual memory is enabled
     // If it is not enabled, just track how much the kernel brk
     // is being raised past the _kernel_orig_brk
-    // if (is_virtual_memory_enabled == 0) {
-
-    // }
+    if (is_virtual_memory_enabled == 0) {
+        
+    } else if (is_virtual_memory_enabled == 1) {
+        KERNEL_BRK = addr;
+    } else {
+        return ERROR;
+    }
     // After VM is enabled, act like standard Brk
     // Enable virtual memory, keeping track of whether the original brk
     // was raised
@@ -34,7 +43,6 @@ extern int SetKernelBrk(void *addr)
 extern void KernelStart(char **cmd_args, unsigned int pmem_size, UserContext *uctxt)
 {
     TracePrintf(0, "\n_____________YOOOO___________ In KernelStart\n");
-
     int i = 0;
     while (cmd_args[i] != NULL)
     {
@@ -43,7 +51,6 @@ extern void KernelStart(char **cmd_args, unsigned int pmem_size, UserContext *uc
     }
 
     TracePrintf(0, "\n pmem_size : %d \n", pmem_size);
-
     TracePrintf(0, "\n UserContext.vector : %d \n", uctxt->vector);
     TracePrintf(0, "\n UserContext.code : %d \n", uctxt->code);
     TracePrintf(0, "\n UserContext.addr : %p \n", uctxt->addr);
@@ -57,8 +64,11 @@ extern void KernelStart(char **cmd_args, unsigned int pmem_size, UserContext *uc
         i++;
     }
 
+    // Initialize up KERNEL_BRK.
+    KERNEL_BRK = _kernel_orig_brk;
+
+
     // Variables.
-    u_long brk_offset = 0; // keep track of allocated stuff.
     n_frames = pmem_size / PAGESIZE;
     TracePrintf(0, "\n n_frames : %d \n", n_frames);
 
@@ -81,54 +91,49 @@ extern void KernelStart(char **cmd_args, unsigned int pmem_size, UserContext *uc
     for (u_long i = VMEM_0_BASE; i < VMEM_0_LIMIT; i += PAGESIZE)
     {
         page_id = i / PAGESIZE;
-        pte_t *page_table_entry = malloc(sizeof(pte_t));
-        if (page_table_entry == NULL)
-        {
-            TracePrintf(0, "\nFatal: failed to allocate bytes. \n");
-        }
-        brk_offset += sizeof(pte_t);
+        pte_t page_table_entry;
 
         // Mark the Kernel Text.
         if (VMEM_0_BASE <= i && i < ((u_long)_kernel_data_start))
         {
-            page_table_entry->valid = 1;
-            page_table_entry->prot = PROT_READ | PROT_EXEC;
+            page_table_entry.valid = 1;
+            page_table_entry.prot = PROT_READ | PROT_EXEC;
             mem_frames.bit_arr[page_id] = 1; // Mark as used.
         }
         // Mark the Kernel Data.
         else if ((u_long)_kernel_data_start <= i && i < (u_long)_kernel_data_end)
         {
-            page_table_entry->valid = 1;
-            page_table_entry->prot = PROT_READ | PROT_WRITE;
+            page_table_entry.valid = 1;
+            page_table_entry.prot = PROT_READ | PROT_WRITE;
             mem_frames.bit_arr[page_id] = 1; // Mark as used.
         }
         // Mark the Kernel Heap.
         else if ((u_long)_kernel_data_end <= i && i < ((u_long)_kernel_orig_brk + brk_offset))
         {
-            page_table_entry->valid = 1;
-            page_table_entry->prot = PROT_READ | PROT_WRITE;
+            page_table_entry.valid = 1;
+            page_table_entry.prot = PROT_READ | PROT_WRITE;
             mem_frames.bit_arr[page_id] = 1; // Mark as used.
         }
         // Mark the Kernel Stack.
         else if (KERNEL_STACK_BASE <= i && i < KERNEL_STACK_LIMIT)
         {
-            page_table_entry->valid = 1;
-            page_table_entry->prot = PROT_READ | PROT_WRITE;
+            page_table_entry.valid = 1;
+            page_table_entry.prot = PROT_READ | PROT_WRITE;
             mem_frames.bit_arr[page_id] = 1; // Mark as used.
         }
         // Mark everything else as invalid.
         else
         {
-            page_table_entry->valid = 0;
-            page_table_entry->prot = 0;
+            page_table_entry.valid = 0;
+            page_table_entry.prot = 0;
         }
 
-        page_table_entry->pfn = page_id;
+        page_table_entry.pfn = page_id;
         kernel_page_table.table[page_id] = page_table_entry;
     }
 
     // Indicate the virtual memory base address of the kernel page table.
-    WriteRegister(REG_PTBR0, (unsigned int)&kernel_page_table);
+    WriteRegister(REG_PTBR0, (unsigned int)&kernel_page_table.table);
 
     // Indicate the number of page table entries.
     WriteRegister(REG_PTLR0, n_kernel_page_table_entries);
@@ -140,24 +145,23 @@ extern void KernelStart(char **cmd_args, unsigned int pmem_size, UserContext *uc
     for (u_long i = VMEM_1_BASE; i < VMEM_1_LIMIT; i += PAGESIZE)
     {
         page_id = i / PAGESIZE;
-        pte_t *page_table_entry = malloc(sizeof(pte_t));
-        brk_offset += sizeof(pte_t);
+        pte_t page_table_entry;
 
         // Mark everything as invalid.
-        page_table_entry->valid = 0;
-        page_table_entry->prot = 0;
-        page_table_entry->pfn = page_id;
+        page_table_entry.valid = 0;
+        page_table_entry.prot = 0;
+        page_table_entry.pfn = page_id;
         region_1_page_table.table[page_id] = page_table_entry;
     }
 
     // Indicate the virtual memory base address of the region 1 page table.
-    WriteRegister(REG_PTBR1, (unsigned int)&region_1_page_table);
+    WriteRegister(REG_PTBR1, (unsigned int)&region_1_page_table.table);
 
     // Indicate the number of page table entries in the region 1 page table.
     WriteRegister(REG_PTLR1, n_region_1_page_table_entries);
 
     // Enable Virtual Memory.
-    // WriteRegister(REG_VM_ENABLE, 1);
+    WriteRegister(REG_VM_ENABLE, 1);
 
     TracePrintf(0, "\n DONSOS CHICOS\n");
 }
