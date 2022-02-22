5be1d58b01b0d3446dcd5f45b7e31f6a59fdf319
Selim Hassairi <selim.hassairi@gmail.com>
2022-02-22 15:01:43 -0500

    testing fork

--- include/utils.h
+++ include/utils.h
@@ -29,12 +29,7 @@ bool search_pcb(void *elementp, const void *keyp);
 
 void remove_parent(void *elementp);
 
-// Stops the current process, adds it to the end of the ready queue,
-// and switches to the next ready process. Default process is idle if no
-// other process is in the ready queue.
-void switch_to_next_ready_process()
-
-    // Set up a way to track free frames (use a bit vector to track this)
-    extern bit_vector_t mem_frames; // 0 for free, 1 for used.
+// Set up a way to track free frames (use a bit vector to track this)
+extern bit_vector_t mem_frames; // 0 for free, 1 for used.
 
 #endif
